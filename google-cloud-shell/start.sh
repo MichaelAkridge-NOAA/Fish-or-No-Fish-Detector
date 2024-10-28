4bc6eb9100ef137ecab1f35404b68e0eb892093c
Michael Akridge - NOAA <65312823+MichaelAkridge-NOAA@users.noreply.github.com>
2024-10-28 07:44:55 -1000

    Update start.sh

--- google-cloud-shell/start.sh
+++ google-cloud-shell/start.sh
@@ -2,7 +2,11 @@
 
 # Attempt to download the SQLite database if it exists
 echo "Checking for existing database..."
-wget -O /app/processed_images.db https://storage.googleapis.com/nmfs_odp_pifsc/PIFSC/ESD/ARP/pifsc-ai-data-repository/fish-detection/MOUSS_fish_detection_v1/datasets/large_2016_dataset/logs/processed_images.db || echo "Database not found, proceeding without it."
+if wget -O /app/processed_images.db https://storage.googleapis.com/nmfs_odp_pifsc/PIFSC/ESD/ARP/pifsc-ai-data-repository/fish-detection/MOUSS_fish_detection_v1/datasets/large_2016_dataset/logs/processed_images.db; then
+    echo "Database downloaded successfully."
+else
+    echo "Database not found, initializing a new one upon processing."
+fi
 
 # Start the Streamlit app
 streamlit run app.py --server.port=8080 --server.enableCORS=false --server.enableXsrfProtection=false
